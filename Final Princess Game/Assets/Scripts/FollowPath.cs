0e5ec345ad9aaf92b58d438c2d3dd960fd5196bc
LHMBeans <65011847+LHMBeans@users.noreply.github.com>
2020-10-18 10:55:10 +0200

    added scripts

---
+++ Final Princess Game/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+    //arraw of waypoints to walk from one to the next one
+    [SerializeField]
+    Transform[] waypoints;
+
+    //Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 3f;
+
+    //Indext of current waypoint from which enemy walks to the next one
+    private int waypointIndex = 0;
+    void Start()
+    {
+        //set position of enemy as position of the first waypoint
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    //method to make the enemy walk
+    void Move()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,
+                                                 waypoints[waypointIndex].transform.position,
+                                                 moveSpeed * Time.deltaTime);
+        if(transform.position == waypoints [waypointIndex].transform.position)
+        {
+            waypointIndex += 1;
+        }
+        if(waypointIndex == waypoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+}
---
+++ Final Princess Game/Assets/Scripts/MovementDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MovementDemo : MonoBehaviour
+{
+    public float movementSpeed = 3f;
+    //public Animator animator;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
