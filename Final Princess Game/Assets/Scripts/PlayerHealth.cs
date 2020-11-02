b5661c5f88aa92e02b1ddf19d9bc2b8f69bd53b2
SkySarah <39993806+SkySarah@users.noreply.github.com>
2020-11-02 12:36:59 +0100

    Princess has sword and which can do movement

--- Final Princess Game/Assets/Scripts/PlayerHealth.cs
+++ Final Princess Game/Assets/Scripts/PlayerHealth.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -8,7 +9,11 @@ public class PlayerHealth : MonoBehaviour
 	public int maxHealth = 100;
 	public int currentHealth;
 
+	private bool fadeOut;
+    public float fadeSpeed;
+
 	public HealthBar healthBar;
+    public GameObject sword;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +25,30 @@ public class PlayerHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.R))
 		{
 			TakeDamage(20);
 		}
+
+		if (currentHealth <= 0)
+        {
+            FadeOutObject();
+			GameOver();
+        }
+
+        if (fadeOut)
+        {
+            Color objectColor = this.GetComponent<Renderer>().material.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            this.GetComponent<Renderer>().material.color = objectColor;
+
+            if (objectColor.a <= 0)
+            {
+                fadeOut = false;
+            }
+        }
     }
 
 	void TakeDamage(int damage)
@@ -31,5 +56,24 @@ public class PlayerHealth : MonoBehaviour
 		currentHealth -= damage;
 
 		healthBar.SetHealth(currentHealth);
+	}
+
+	void FadeOutObject()
+    {
+        fadeOut = true;
+        sword.SetActive(false);
+    }
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Sword")
+        {
+            TakeDamage(20);
+        }
+    }
+
+	void GameOver()
+	{
+		SceneManager.LoadScene("Menu");
 	}
 }
